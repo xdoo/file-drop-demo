9fdc7a167aadd8ae6664f2fc91b5db8278f3cef9
xdoo <claus.straube@gmail.com>
2020-03-26 14:41:25 +0100

    base config

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="black"
+      dark
+    >
+      <div class="d-flex align-center">
+        <span class="headline font-weight-black">FileDrop</span><span class="headline font-weight-light">Demo</span>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
+export default Vue.extend({
+  name: 'App',
+
   components: {
     HelloWorld,
   },
-})
-export default class App extends Vue {}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+});
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fluid fill-height>
+    <v-row class="text-center">
+      <v-col cols="12">
+         <input 
+      type="file" 
+      id="fileUpload" 
+      accept="text/xml" 
+      style="display:none">
+    <v-sheet
+      ref="dropzone"
+      id="dropzone"
+      title="Klicken Sie hier, um eine Datei von Ihrem Rechner auszuwählen."
+      color="black"
+      width="100%"
+      style="cursor:pointer;border-style: solid;border-width: 4px;border-color: black;"
+      height="250"
+    >
+      <v-col class="white--text">
+          <v-row justify="center" v-if="!loading">
+            <v-icon
+              v-if="!dragover" 
+              :color="fileDropIconColor" 
+              :size="iconSize"
+              >mdi-cloud-upload-outline</v-icon>
+            <v-icon
+              v-if="dragover" 
+              :color="fileDropIconColor" 
+              :size="iconSize"
+              >mdi-book-plus</v-icon>
+          </v-row>
+          <v-row justify="center" v-if="!loading">
+            <span :class="uploadSize">Select or drop your file here!</span>
+          </v-row>
+          <v-row justify="center" v-if="loading">
+            <v-progress-circular
+              size="60"
+              width="15"
+              color="white"
+              indeterminate
+            ></v-progress-circular>
+          </v-row>
+          <v-row justify="center" v-if="loading">
+            <span :class="uploadSize">Datei wird verarbeitet</span>
+          </v-row>
+        </v-col>
+    </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+  import Vue from "vue"
+  import { Component, Watch, Prop, Ref } from "vue-property-decorator"
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+  @Component
+  export default class HelloWorld extends Vue {
+    loading: boolean = false
+    dragover: boolean = false
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
